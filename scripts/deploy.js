5f2c0b616927eb5f1ac1af015642ca9cd8a8e316
ametel01 <alex-metelli@gmx.com>
2021-10-18 23:42:57 +0100

    started app.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Lottery = await hre.ethers.getContractFactory("Lottery");
+  const lottery = await Lottery.deploy(60);
 
-  await greeter.deployed();
+  await lottery.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Greeter deployed to:", lottery.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState }  from 'react';
+import { ethers } from 'ethers';
+import Lottery from './artifacts/contracts/Lottery.sol/Lottery.json';
+
+const lotteryAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 function App() {
+
+  const [owner, expiration] = useState()
+
+  async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' })
+  }
+
+  async function fetchOwner() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const contract = new ethers.Contract(lotteryAddress, Lottery.abi, provider)
+      try {
+        const owner = await contract.owner()
+        console.log('owner: ', owner.toString())
+      }  catch (err) {
+        console.log('Error: ', err)
+      }
+    }
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={fetchOwner}>Fetch Owner</button>
       </header>
     </div>
   );
